6b6e71f6bf5f196c4db14ef4cc6906299893ce7b
Aaron Qian <aq1018@gmail.com>
2013-01-17 11:30:13 -0800

    adding specs for emulation

---
+++ spec/lib/active_model/error_collecting/emulation_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+shared_examples_for "a delegated method" do
+  let(:target_instance) { mock() }
+  before do
+    target_instance.should_receive method
+    instance.stub(target).and_return(target_instance)
+  end
+
+  specify { instance.send method }
+end
+
+describe ActiveModel::ErrorCollecting::Emulation do
+  subject(:instance) { klass.new }
+  let(:klass) { Class.new { include ActiveModel::ErrorCollecting::Emulation } }
+
+  delegation_map = {
+    error_collection: [
+      :clear, :include?, :get, :set, :delete, :[], :[]=, :each, :size,
+      :values, :keys, :count, :empty?, :added?, :add
+    ],
+
+    human_reporter: [
+      :full_messages, :full_message, :generate_messages
+    ],
+
+    hash_reporter: [
+      :to_hash
+    ],
+
+    hash_reporter: [
+      :to_hash
+    ]
+  }
+
+  delegation_map.each do |target, methods|
+    methods.each do |method|
+      describe "delegating ##{method} to  ##{target}" do
+        let(:target) { target }
+        let(:method) { method }
+        it_should_behave_like "a delegated method"
+      end
+    end
+  end
+end
