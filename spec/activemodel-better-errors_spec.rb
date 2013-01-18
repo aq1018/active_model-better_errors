38545191f47bbde39bf6e473189231d2279f0868
Aaron Qian <aq1018@gmail.com>
2013-01-17 18:07:02 -0800

    testing the hook.?

--- spec/activemodel-better-errors_spec.rb
+++ spec/activemodel-better-errors_spec.rb
@@ -1,31 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe ActiveModel::ErrorCollecting do
-  # describe "numericality check" do
-  #   let(:ruler) { Ruler.new(length: 13) }
-  #   subject(:errors) { ruler.errors }
-  #   before { ruler.valid? }
-
-  #   describe "for human consumer" do
-  #     its(:full_messages) { should == ["Length must be less than or equal to 12"] }
-  #   end
-
-  #   describe "json reporter" do
-  #     its(:to_hash) { should == { :length => [[:less_than_or_equal_to, options]] } }
-  #   end
-  # end
-
-  # describe "presence check" do
-  #   let(:user) { User.new }
-  #   subject(:errors) { user.errors }
-  #   before { user.valid? }
-
-  #   describe "for human consumer" do
-  #     its(:full_messages) { should == ["First name plz...?", "Last name plz...?"] }
-  #   end
-
-  #   describe "json reporter" do
-  #     its(:to_hash) { should == { :first_name => [:blank], :last_name => [:blank] } }
-  #   end
-  # end
+describe "ActiveModel Better Errors" do
+  it "overrides ActiveModel Validations" do
+    User.new.errors.should be_a ActiveModel::ErrorCollecting::Errors
+  end
 end
