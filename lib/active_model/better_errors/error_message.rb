db8b66d47b357e23cf8cabbe60c28655fb500a3e
Aaron Qian <aq1018@gmail.com>
2014-01-13 21:59:42 -0800

    remove old logic from ErrorMessage. They now reside in ErrorMessage::Builder

--- lib/active_model/better_errors/error_message.rb
+++ lib/active_model/better_errors/error_message.rb
@@ -8,48 +8,6 @@ module ActiveModel
     class ErrorMessage
       include Helper, Comparable
 
-      CALLBACKS_OPTIONS = [
-        :if, :unless, :on, :allow_nil, :allow_blank, :strict
-      ]
-
-      # return the message either as nil, symbol, or string
-      def self.normalize(message)
-        case message
-        when nil
-          nil
-        when Symbol
-          message
-        when Proc
-          message.call
-        else
-          message.to_s
-        end
-      end
-
-      def self.identify(message, override)
-        message   = normalize message
-        override  = normalize override
-
-        symbol = message if message.is_a?(Symbol)
-        symbol = override if override.is_a?(Symbol)
-
-        string = message if message.is_a?(String)
-        string = override if override.is_a?(String)
-
-        string = nil if string.blank?
-
-        [symbol, string]
-      end
-
-      def self.build(base, attribute, message, options = nil)
-        options   = options ? options : {}
-        options   = options.except(*CALLBACKS_OPTIONS)
-
-        symbol, string = identify message, options.delete(:message)
-
-        new(base, attribute, symbol, string, options)
-      end
-
       attr_reader :base, :attribute, :type, :message, :options
 
       def initialize(base, attribute, type, message, options = {})
